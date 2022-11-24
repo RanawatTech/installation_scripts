2e8079dd50a3ddc4e688eaa616867ea383c62718
Govind Singh Ranawat <govindkrsingh0605@gmail.com>
2022-11-25 01:21:24 +0530

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -14,3 +14,45 @@ To install Ansible:
     3  sudo amazon-linux-extras install epel
     4  yum install git python python-level python.pp openssl ansible -y
     5  yum update -y
+
+
+###########################################
+Module
+---------
+ ansible demo -b -m yum -a "pkg=httpd state=absent"
+ 
+  demo=Group name
+ -b =sudo previlage
+ -m =module
+ 
+ 
+ Copy file from server to node
+  ansible demo[0] -b -m copy -a "src=copiedfromserver dest=/temp"
+
+
+
+//
+ansible demo -m setup
+
+
+
+///
+
+
+#############################################################
+---
+- hosts: demo
+  user: ansible
+  become: yes
+  connection: ssh
+  vars:
+    pkgname: httpd
+  tasks:
+            - name: install HTTPD server on centos 7
+              action: yum name='{{pkgname}}' state=installed
+              
+        
+###############################################################        
+~
+
+ansible demo -m setup -a "filter=*ipv4*"
